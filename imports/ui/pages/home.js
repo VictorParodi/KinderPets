88f830af12b12f6ce4036dd0832c0a89f54c0105
VictorParodi <victor.parodi.montero@gmail.com>
2016-08-15 18:37:17 -0500

    Rama Master commit 4: header home listo

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -4,6 +4,18 @@ import { Template } from 'meteor/templating';
 /* ============================== importing home html template ============================== */
 import './home.html';
 
+
+/* ============================== Hooks ============================== */
 Template.home.onRendered( () => {
   //$('body').css('background-image', 'url("fondo5.jpg")');
+});
+
+
+/* ============================== Events ============================== */
+Template.home.events({
+  'click .js-event-anchor-nav'(e) {
+    e.preventDefault();
+    let message = $(e.target).html();
+    alert(`Hola ${message}`);
+  }
 });
