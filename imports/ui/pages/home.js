e7a0b557de374ad73e3aae38b9c248f588f0a513
VictorParodi <victor.parodi.montero@gmail.com>
2016-08-15 00:12:40 -0500

    Rama Master commit 3: header home. UIkit agregado

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -3,3 +3,7 @@ import { Template } from 'meteor/templating';
 
 /* ============================== importing home html template ============================== */
 import './home.html';
+
+Template.home.onRendered( () => {
+  //$('body').css('background-image', 'url("fondo5.jpg")');
+});
